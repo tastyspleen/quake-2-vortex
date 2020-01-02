abe3cfce3c226e5b67086ceececbce2388da90af
QwazyWabbit <gandalf.2004@gmail.com>
2020-01-01 18:30:54 -0800

    Delete all references to Q_fabs.

--- q_shared.h
+++ q_shared.h
@@ -158,9 +158,6 @@ extern vec3_t vec3_origin;
 
 #define	IS_NAN(x) (((*(int *)&x)&nanmask)==nanmask)
 
-// microsoft's fabs seems to be ungodly slow...
-//float Q_fabs (float f);
-//#define	fabs(f) Q_fabs(f)
 #if !defined C_ONLY
 extern long Q_ftol( float f );
 #else
