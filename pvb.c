03eab153653227dd874c9022fc7eea911a805891
QwazyWabbit <gandalf.2004@gmail.com>
2020-01-06 15:49:35 -0800

    Fix uninitialized exp_points. Function was using garbage value.

--- pvb.c
+++ pvb.c
@@ -228,7 +228,9 @@ void AddBossExp (edict_t *attacker, edict_t *target)
 
 void AwardBossKill (edict_t *boss)
 {
-	int			i, damage, exp_points, credits;
+	int			i, damage;
+	int			exp_points = PVB_BOSS_FRAG_EXP;
+	int			credits;
 	float		levelmod, dmgmod;
 	char		*message;
 	edict_t		*player;
